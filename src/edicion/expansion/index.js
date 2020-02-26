18d709341b2a4fae51a7b394e007f467d3dbacfa
Brankitocruz <dafnemus@gmail.com>
2020-02-26 14:10:23 -0300

    termine la funcionalidad del video modal

--- src/edicion/expansion/index.js
+++ src/edicion/expansion/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import cruzar from './cruzar.svg';
+import './style.css'
 
 
 class Video extends React.Component{
@@ -7,8 +9,8 @@ class Video extends React.Component{
       return(
         
         <div>
-                  <iframe width="560" height="315" src="https://www.youtube.com/embed/U0Q78L6rmwE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-              <button onClick={this.props.closeModal}>X</button>
+                  <iframe width="560" height="315" src="https://www.youtube.com/embed/U0Q78L6rmwE" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen/>
+              <button className="cerrar" onClick={this.props.closeModal}><img src={cruzar} alt="icono"/></button>
         </div>
       );
     }
